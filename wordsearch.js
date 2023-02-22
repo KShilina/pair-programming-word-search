9a3e618202ddbd2f15f8f4a6833d373f8d0bc9e0
Ekaterina Shilina <yatakshilina@gmail.com>
2023-02-22 15:35:40 -0500

    first commit.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length < 1 || word.length < 1) return false; //check if word exist
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  for (let i = 0; i < letters.length - 1; i++) {
+    const arr1 = [];
+    for (let j = 0; j < letters.length; j++) {
+      arr1.push(letters[j][i]);
+    }
+    const newStr = arr1.join(""); //string
+    if (newStr.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
